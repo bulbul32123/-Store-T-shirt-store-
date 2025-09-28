059004e76d7ae621bd17f70c131d56629ebd4b7f
bulbul32123 <bulbultheprogrammer@gmail.com>
2025-09-28 21:43:16 +0600

    components ui folder added

---
+++ frontend/src/components/ui/dropdown/Dropdown.js
@@ -0,0 +1,42 @@
+"use client";
+import React from "react";
+import { useEffect, useRef } from "react";
+
+
+export const Dropdown = ({
+  isOpen,
+  onClose,
+  children,
+  className = "",
+}) => {
+  const dropdownRef = useRef(null);
+
+ useEffect(() => {
+  const handleClickOutside = (event) => {
+    if (
+      dropdownRef.current &&
+      !dropdownRef.current.contains(event.target ) &&
+      !(event.target ).closest('.dropdown-toggle')
+    ) {
+      onClose();
+    }
+  };
+
+  document.addEventListener("mousedown", handleClickOutside);
+  return () => {
+    document.removeEventListener("mousedown", handleClickOutside);
+  };
+}, [onClose]);
+
+
+  if (!isOpen) return null;
+
+  return (
+    <div
+      ref={dropdownRef}
+      className={`absolute z-40 right-0 mt-3 rounded-xl border border-gray-200 bg-white ${className}`}
+    >
+      {children}
+    </div>
+  );
+};
