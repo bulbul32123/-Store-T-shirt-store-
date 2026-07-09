71bc7db558cad3afdf78532e54f215b2869c2b3a
Bulbul Islam <mdbulbulislamtheprogrammer@gmail.com>
2026-07-09 14:14:11 +0600

    updated src/lib/socket.js

---
+++ frontend/src/lib/socket.js
@@ -0,0 +1,16 @@
+// /lib/socket.js on frontend
+'use client';
+import { io } from 'socket.io-client';
+import { API_URL } from '../utils/config';
+
+let socket = null;
+
+export const getSocket = () => {
+    if (!socket) {
+        socket = io(API_URL, {
+            withCredentials: true,
+            autoConnect: false,
+        });
+    }
+    return socket;
+};
