3e0bf28395fb8ecff2e72bf5b62ac29448eee6ce
Bulbul Islam <mdbulbulislamtheprogrammer@gmail.com>
2026-07-17 23:49:44 +0600

    updated

--- backend/utils/cloudinary.js
+++ backend/utils/cloudinary.js
@@ -1,30 +1,34 @@
-const cloudinary = require('cloudinary').v2;
-const { CLOUDINARY_CONFIG } = require('../config');
+// utils/cloudinary.js
+const cloudinary = require("cloudinary").v2;
+const { CLOUDINARY_CONFIG } = require("../config");
 
 cloudinary.config(CLOUDINARY_CONFIG);
 
-exports.uploadToCloudinary = async (filePath) => {
-    try {
-        const result = await cloudinary.uploader.upload(filePath, {
-            folder: 'tshirt-store'
-        });
+exports.uploadToCloudinary = async (
+  filePath,
+  folder = "payra-store/assets",
+) => {
+  try {
+    const result = await cloudinary.uploader.upload(filePath, {
+      folder,
+    });
 
-        return {
-            url: result.secure_url,
-            public_id: result.public_id
-        };
-    } catch (error) {
-        console.error('Cloudinary upload error:', error);
-        throw new Error('Error uploading image to Cloudinary');
-    }
+    return {
+      url: result.secure_url,
+      public_id: result.public_id,
+    };
+  } catch (error) {
+    console.error("Cloudinary upload error:", error);
+    throw new Error("Error uploading image to Cloudinary");
+  }
 };
 
 exports.deleteFromCloudinary = async (publicId) => {
-    try {
-        await cloudinary.uploader.destroy(publicId);
-        return { success: true };
-    } catch (error) {
-        console.error('Cloudinary delete error:', error);
-        throw new Error('Error deleting image from Cloudinary');
-    }
-}; 
+  try {
+    await cloudinary.uploader.destroy(publicId);
+    return { success: true };
+  } catch (error) {
+    console.error("Cloudinary delete error:", error);
+    throw new Error("Error deleting image from Cloudinary");
+  }
+};
