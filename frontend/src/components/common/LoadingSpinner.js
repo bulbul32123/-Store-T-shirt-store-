d94f4e81e8026a09ae86b01e0d6f70de02404527
Bulbul Islam <mdbulbulislamtheprogrammer@gmail.com>
2026-07-20 22:19:31 +0600

    LoadingSpinner.js updated

--- frontend/src/components/common/LoadingSpinner.js
+++ frontend/src/components/common/LoadingSpinner.js
@@ -1,7 +1,7 @@
 const LoadingSpinner = () => {
  
     return (
-      <div className="min-h-screen flex items-center justify-center">
+      <div className="flex items-center justify-center">
         <div className="h-8 w-8 rounded-full border-2 border-[#111] border-t-transparent animate-spin" />
       </div>
     );
