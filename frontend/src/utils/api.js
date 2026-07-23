6b12262bef4b7f3f0e264860d03098509518fd20
Bulbul Islam <mdbulbulislamtheprogrammer@gmail.com>
2026-07-23 16:17:54 +0600

    uoated

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import { API_URL } from './config';
 const api = axios.create({
-    baseURL: '',
+    baseURL: API_URL,
     withCredentials: true,
     headers: {
         'Content-Type': 'application/json',
